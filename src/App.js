ebb207cc8ea1f0d1a179b430e27fa3a7415dd02f
simon.horrabin <simonhorrabin@gmail.com>
2023-07-20 19:42:46 -0400

    app.js completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Header from './components/header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Experience from "./components/experience/experience";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience/>
+            <Services />
+            <Portfolio />
+            <Contact />
+            <Footer />
+        </>
+    );
+};
 
 export default App;
